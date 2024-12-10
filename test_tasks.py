77881e89bf32318cd58d129a45f06b287c817887
ealucas <ealucas@github.com>
2024-12-09 21:45:44 -0300

    more tests

--- test_tasks.py
+++ test_tasks.py
@@ -13,6 +13,9 @@ class TestTask(unittest.TestCase):
             self.assertFalse(task.completed)
             task.mark_done()
             self.assertTrue(task.completed)
-            
+    def test_set_due(self):
+            task = Task(*"Test Task", priority="Low", deadline="2024-12-21")
+            task.set_due("2025-12-21")
+            task.assertEqual(task.deadline, "2025-12-21")
 if __name__ == '__main__':
     unittest.main()
