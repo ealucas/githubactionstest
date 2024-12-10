921d5711503347e8886e3ddfcb04e3738de95e6a
ealucas <ealucas@github.com>
2024-12-09 21:28:34 -0300

    Update test_tasks

--- test_tasks.py
+++ test_tasks.py
@@ -1,10 +1,13 @@
 import unittest
 from main import *
+class TestTask(unittest.TestCase):
+    def test_initialization(self):
+            task = Task("Test Task", priority="High", deadline="2024-12-31")
+            self.assertEqual(task.description, "Test Task")
+            self.assertEqual(task.priority, "High")
+            self.assertEqual(task.deadline, "2024-12-31")
+            self.assertFalse(task.completed)
 
-def test_initialization(self):
-        task = Task("Test Task", priority="High", deadline="2024-12-31")
-        self.assertEqual(task.description, "Test Task")
-        self.assertEqual(task.priority, "High")
-        self.assertEqual(task.deadline, "2024-12-31")
-        self.assertFalse(task.completed)
 
+if __name__ == '__main__':
+    unittest.main()
